c26a2897463b6b11a1a3ba87e825caadda637544
demianmln <demianmolina27@gmail.com>
2024-04-14 11:54:24 -0300

    DOMDesafioCompletado

--- index.js
+++ index.js
@@ -24,7 +24,32 @@ const cosasQueAprendimos = [
     class: "special",
   },
 ];
-
-function main() {}
-
+function main() {
+  const listaUl = document.querySelector(".lista");
+  // console.log("listaUl: ", listaUl);
+  const child = listaUl.querySelectorAll("li");
+  // console.log("losLi: ", child);
+  child.forEach((elemento) => {
+    elemento.remove();
+  });
+  cosasQueAprendimos.forEach((objeto) => {
+    let nuevoLi = document.createElement("li");
+    nuevoLi.textContent = objeto.tema;
+    nuevoLi.classList.add(objeto.class || "item");
+    listaUl.appendChild(nuevoLi);
+  });
+}
 main();
+
+//   const listaUl = document.querySelector(".lista");
+//   console.log("listaUl: ", listaUl);
+//   listaUl.innerHTML = ""; // Eliminar elementos li existentes
+//   cosasQueAprendimos.forEach((objetoActual) => {
+//     const nuevoElementoLi = document.createElement("li");
+//     nuevoElementoLi.textContent = objetoActual.tema;
+//     if (objetoActual.class) {
+//       nuevoElementoLi.className = objetoActual.class;
+//     }
+//     listaUl.appendChild(nuevoElementoLi);
+//   });
+// }
